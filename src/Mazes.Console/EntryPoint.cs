f8e4e67a1bd9d32946a13a1855b57f576c477100
Stevo John <stevehuwjohn@gmail.com>
2026-06-14 18:09:15 +0100

    Implementing

---
+++ src/Mazes.Console/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using CommandLine;
+using Mazes.Console.Infrastructure;
+using Mazes.Console.Runners;
+
+namespace Mazes.Console;
+
+public static class EntryPoint
+{
+    public static void Main(string[] arguments)
+    {
+        var parser = new Parser(settings =>
+        {
+            settings.CaseInsensitiveEnumValues = true;
+            settings.HelpWriter = System.Console.Out;
+        });
+
+        parser.ParseArguments<RemoteOptions>(arguments)
+            .WithParsed(options => new Remote().Run(options));
+    }
+}
---
+++ src/Mazes.Console/Runners/Remote.cs
@@ -0,0 +1,10 @@
+using Mazes.Console.Infrastructure;
+
+namespace Mazes.Console.Runners;
+
+public class Remote
+{
+    public void Run(RemoteOptions options)
+    {
+    }
+}
