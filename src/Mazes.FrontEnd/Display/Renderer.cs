bce92f0a6398bb6b0b67b8b19829c7683f0fc860
Stevo John <stevehuwjohn@gmail.com>
2026-06-14 22:10:55 +0100

    Implementing

---
+++ src/Mazes.FrontEnd/Display/Renderer.cs
@@ -0,0 +1,8 @@
+using Microsoft.Xna.Framework;
+
+namespace Mazes.FrontEnd.Display;
+
+public class Renderer : Game
+{
+    
+}
---
+++ src/Mazes.FrontEnd/EntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using Mazes.FrontEnd.Display;
+
+namespace Mazes.FrontEnd;
+
+public static class EntryPoint
+{
+    public static void Main()
+    {
+        var renderer = new Renderer();
+        
+        renderer.Run();
+    }
+}
