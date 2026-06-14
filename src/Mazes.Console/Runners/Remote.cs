55190a373ed30260c2ce51e5693331f2270eb21f
Stevo John <stevehuwjohn@gmail.com>
2026-06-14 18:14:47 +0100

    Implementing

--- src/Mazes.Console/Runners/Remote.cs
+++ src/Mazes.Console/Runners/Remote.cs
@@ -1,4 +1,7 @@
 using Mazes.Console.Infrastructure;
+using Mazes.Core.Infrastructure;
+using Mazes.Core.Models;
+using static System.Console;
 
 namespace Mazes.Console.Runners;
 
@@ -6,5 +9,90 @@ public class Remote
 {
     public void Run(RemoteOptions options)
     {
+        var validationResult = options.Validate();
+
+        if (! validationResult.IsValid)
+        {
+            WriteLine($"\n{validationResult.Message}\n");
+
+            return;
+        }
+
+        var client = new PuzzleClient();
+
+        Clear();
+
+        var startTime = DateTime.Now;
+
+        for (var i = 0; i < options.Quantity; i++)
+        {
+            WriteLine();
+
+            WriteLine($"Fetching {options.Difficulty.ToString().ToLowerInvariant()} maze {i + 1:N0} of {options.Quantity:N0}...");
+
+            WriteLine();
+
+            (DateOnly Date, Maze Maze)? puzzle = null;
+
+            for (var retry = 1; retry < 21; retry++)
+            {
+                try
+                {
+                    puzzle = options.Day > 0
+                        ? client.GetPuzzle(options.Difficulty, new DateOnly(options.Year.Value, options.Month.Value, options.Day.Value))
+                        : client.GetNextPuzzle(options.Difficulty);
+                }
+                catch
+                {
+                    //
+                }
+
+                if (puzzle != null)
+                {
+                    break;
+                }
+
+                var sleep = (int) Math.Pow(retry, 2);
+
+                for (var timer = 0; timer < sleep; timer++)
+                {
+                    if (retry > 1)
+                    {
+                        CursorTop -= 2;
+                    }
+
+                    WriteLine($"Waiting for {sleep - timer:N0}s before attempt {retry}.  ");
+
+                    WriteLine();
+
+                    Thread.Sleep(1_000);
+
+                    CursorTop -= 2;
+
+                    WriteLine("Retrying...                         ");
+
+                    WriteLine();
+                }
+            }
+        
+            Clear();
+
+            if (puzzle == null)
+            {
+                WriteLine("No puzzles available.");
+
+                break;
+            }
+
+            WriteLine();
+            
+            WriteLine($@"Started: {startTime:F}, runtime: {DateTime.Now - startTime:h\:mm\:ss\.fff}.");
+
+            WriteLine();
+            
+            WriteLine($"Solving {options.Difficulty.ToString().ToLowerInvariant()} puzzle for {puzzle.Value.Date:R} ({puzzle.Value.Maze.Width}x{puzzle.Value.Maze.Height}). {i + 1:N0} / {options.Quantity:N0}.");
+
+            WriteLine();
+        }
     }
 }
--- src/Mazes.Core/Models/Maze.cs
+++ src/Mazes.Core/Models/Maze.cs
@@ -2,6 +2,10 @@ namespace Mazes.Core.Models;
 
 public class Maze
 {
+    public int Width { get; }
+    
+    public int Height { get; }
+    
     public Maze(Puzzle puzzle)
     {
     }
