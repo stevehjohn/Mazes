797eab0656c3830bcc5199f88ea30c0389aec8c4
Stevo John <stevehuwjohn@gmail.com>
2026-06-14 18:01:38 +0100

    Implementing

--- src/Mazes.Core/Models/Data.cs
+++ src/Mazes.Core/Models/Data.cs
@@ -1,6 +1,15 @@
+using JetBrains.Annotations;
+
 namespace Mazes.Core.Models;
 
+[UsedImplicitly]
 public class Data
 {
+    public int[] HorizontalWalls { get; set; }
     
+    public int[] VerticalWalls { get; set; }
+    
+    public int StartingTile { get; set; }
+    
+    public int FinishTile { get; set; }
 }
