6a0ea5372d4dd9574eb344e9b9c89c89093256af
Stevo John <stevehuwjohn@gmail.com>
2026-06-30 14:33:43 +0100

    Noice

--- src/Mazes.FrontEnd/Infrastructure/Constants.cs
+++ src/Mazes.FrontEnd/Infrastructure/Constants.cs
@@ -10,7 +10,7 @@ public static class Constants
 
     public static Color BackgroundColour { get; } = Color.Black;
 
-    public static Color WallColour { get; } = new(64, 64, 64);
+    public static Color WallColour { get; } = new(0xE0, 0xE0, 0xE0, 0xFF);
 
     public static Color PathColour { get; } = new(0xFF, 0xB0, 0x00, 0xFF);
 
