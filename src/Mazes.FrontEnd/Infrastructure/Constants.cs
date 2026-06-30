ee45766a8b84dc8496997b99a7c1385781808bd9
Stevo John <stevehuwjohn@gmail.com>
2026-06-30 15:49:44 +0100

    Code clean

--- src/Mazes.FrontEnd/Infrastructure/Constants.cs
+++ src/Mazes.FrontEnd/Infrastructure/Constants.cs
@@ -14,7 +14,7 @@ public static class Constants
 
     public static Color BackgroundColour { get; } = Color.Black;
 
-    public static Color WallColour { get; } = new(0xE0, 0xE0, 0xE0, 0xFF);
+    public static Color WallColour { get; } = Color.Lime;
 
     public static Color PathColour { get; } = new(0xFF, 0xB0, 0x00, 0xFF);
 
