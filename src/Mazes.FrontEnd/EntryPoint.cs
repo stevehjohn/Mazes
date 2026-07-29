242c2b21f5e6d2c905d31441197953a19edbbf81
Stevo John <stevehuwjohn@gmail.com>
2026-07-29 19:58:43 +0100

    Code clean

--- src/Mazes.FrontEnd/EntryPoint.cs
+++ src/Mazes.FrontEnd/EntryPoint.cs
@@ -8,7 +8,6 @@ public static class EntryPoint
 {
     public static void Main(string[] arguments)
     {
-        Console.WriteLine($"Args: {string.Join(", ", arguments)}");
         var renderer = arguments.Length > 0
             ? new Renderer(Enum.Parse<Difficulty>(arguments[0], true))
             : new Renderer(Difficulty.Small);
